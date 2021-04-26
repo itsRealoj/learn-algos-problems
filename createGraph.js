3029355dce7557a51e72e45265b5827074f43096
itsrealoj <stephenojwang1040@gmail.com>
2021-04-26 06:46:35 +0300

    remove vertex from a graph

--- createGraph.js
+++ createGraph.js
@@ -13,15 +13,25 @@ class Graph {
         this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
         this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1)
     }
+    removeVertex(vertex) {
+        this.adjacencyList[vertex].forEach(edge => {
+            this.removeEdge(edge, vertex)
+        })
+        delete this.adjacencyList[vertex]
+        
+    }
 }
 
 const g = new Graph()
 g.addVertex('MIN')
 g.addVertex('TES')
+g.addVertex('TOK')
 g.addVertex('CAL')
 g.addEdge('TES', 'MIN')
+g.addEdge('TES', 'TOK')
+g.addEdge('MIN', 'TOK')
 g.addEdge('MIN', 'CAL')
 console.log(g)
-
-g.removeEdge('MIN', 'CAL')
+g.removeVertex('TES')
+// g.removeEdge('MIN', 'CAL')
 console.log(g)
