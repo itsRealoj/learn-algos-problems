cb7a19c7f88b8c57cc9d29f84d988d54fab21638
itsrealoj <stephenojwang1040@gmail.com>
2021-04-26 06:25:59 +0300

    remove edge from a graph

--- createGraph.js
+++ createGraph.js
@@ -1,18 +1,27 @@
 class Graph {
     constructor() {
-        this.vertex = {}
+        this.adjacencyList = {}
     }
-    addVertex(name){
-        return this.vertex[name] = []
+    addVertex(vertex){
+        return this.adjacencyList[vertex] = []
     }
-    addVal(vert, val){
-        this.vertex[vert].push(val)
+    addEdge(v1, v2){
+        this.adjacencyList[v1].push(v2)
+        this.adjacencyList[v2].push(v1)
+    }
+    removeEdge(v1, v2){
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1)
     }
 }
 
 const g = new Graph()
-g.addVertex('Min')
+g.addVertex('MIN')
 g.addVertex('TES')
 g.addVertex('CAL')
-g.addVal('Min', 10)
+g.addEdge('TES', 'MIN')
+g.addEdge('MIN', 'CAL')
+console.log(g)
+
+g.removeEdge('MIN', 'CAL')
 console.log(g)
