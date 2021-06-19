8611566e636f88ec4f9659ae17f0e488464f1da5
itsrealoj <stephenojwang1040@gmail.com>
2021-06-20 01:42:07 +0300

    two sum correct solution with hash

--- twoSum.js
+++ twoSum.js
@@ -1,22 +1,27 @@
+/*
+PATTERN: FREQUENCY COUNTER METHOD
+
+-create empty object
+-loop through the array, check if the the number you expect to add to the the currrent number to reach the target is available in the hash obj.
+-if not create current value to the obj with its index.
+-if you found the partner in the array, retrun the value of the number found in  the obj, and the current index of the value from the array.
+*/
 const twoSum = (nums, target) => {
-    let newNumsArr = [...nums];
-    console.log(newNumsArr)
-    let l = nums.length - 1;
-    let f = 0;
-
-    nums.sort((a,b) => a -b);
-    console.log(newNumsArr)
+    let obj = {}
+    let index = 0;
     
-    while(l > f) {
-        if(nums[f] + nums[l] === target) {
-            console.log('sum>>', [newNumsArr.indexOf(nums[f]), newNumsArr.indexOf(nums[l])]);
-            return [newNumsArr.indexOf(nums[f]), newNumsArr.indexOf(nums[l])];
-        } else if(nums[f] + nums[l] > target) {
-            l -= 1;
-        } else if(nums[f] + nums[l] < target) {
-            f += 1;
-            }
+    for(let num of nums)  {
+        console.log(typeof obj[target - num])
+        if(typeof obj[target - num] === 'number') {
+            console.log([obj[target - num], index])
+            return [obj[target - num], index];
+        } else {
+            obj[num] = index;
+            console.log(obj)
+            index += 1;
+        }
     }
-}
+    return 'no two sum!'
 
-twoSum([3,2,4], 6)
+}
+twoSum([2,11,15,7], 9)
